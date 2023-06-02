496f95cc0c80da5d03712841c988ff494608d89f
Gabriel <gabriel.rouge@etu.univ-amu.fr>
2023-06-02 14:49:20 +0200

    Implémentation de la fonction draw

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -1,38 +1,44 @@
 package fr.amu.iut.cc3;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ToileController implements Initializable {
     @FXML
-    TextField comp1;
-    @FXML
-    TextField comp2;
-    @FXML
-    TextField comp3;
-    @FXML
-    TextField comp4;
-    @FXML
-    TextField comp5;
-    @FXML
-    TextField comp6;
+    Pane pane;
     private static int rayonCercleExterieur = 200;
     private static int angleEnDegre = 60;
     private static int angleDepart = 90;
     private static int noteMaximale = 20;
 
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
     }
 
+    public ToileController () {
+        pane = new Pane();
+    }
+
     @FXML
-    void draw(){
-        System.out.println("Check");
+    void draw(ActionEvent event){
+        // Get event informations
+        TextField source = (TextField) event.getSource();
+        int value = Integer.parseInt(source.getText());
+        int axe = Integer.parseInt(String.valueOf(source.getId().charAt(4)));
+
+        // Draw the circle at the right location
+        Circle circle = new Circle();
+        circle.setCenterX(getXRadarChart(value, axe));
+        circle.setCenterY(getYRadarChart(value, axe));
+        circle.setRadius(6);
+        pane.getChildren().add(circle);
     }
 
     int getXRadarChart(double value, int axe ){
